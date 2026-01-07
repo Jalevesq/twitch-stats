160047ba73ee6dafd88a8f8b0ca77fa2f2e13555
jacob <jacob25.levesque1@outlook.com>
2026-01-07 06:00:27 -0500

    display username

--- website/app/(protected)/dashboard/page.tsx
+++ website/app/(protected)/dashboard/page.tsx
@@ -3,11 +3,11 @@
 import { useRequiredSession } from "@/app/_context/session-context";
 
 export default function Dashboard() {
-  // const session = useRequiredSession();
+  const session = useRequiredSession();
 
   return (
     <main className="flex-1 px-12 py-8">
-      <h1 className="text-3xl font-display mb-4">Welcome, {"abc"}!</h1>
+      <h1 className="text-3xl font-display mb-4">Welcome, {session.user.displayName}!</h1>
       <p className="text-text-secondary">This is your dashboard.</p>
     </main>
   );

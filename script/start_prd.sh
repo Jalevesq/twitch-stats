7024aaf103a31ae9d2afe26f35bf88472f5d2681
jacob <jacob25.levesque1@outlook.com>
2026-01-07 10:47:04 -0500

    start prd script

---
+++ script/start_prd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")/../docker-compose"
+
+echo "📦 Pulling latest code..."
+git pull
+
+echo "🐳 Pulling latest images..."
+docker compose -f docker-compose.prd.yml pull
+
+echo "🚀 Starting app..."
+docker compose -f docker-compose.prd.yml up -d
+
+echo "✅ Deployed!"
